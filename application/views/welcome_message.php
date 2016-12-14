d319c8bc2437a103dc96142d7e5b4da3c04a950b
JoseSoto33 <jose.soto.bit@gmail.com>
2016-12-13 20:14:34 -0430

    Modificacion de la pagina de inicio

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -68,21 +68,21 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <body>
 
 <div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
+	<h1>Bienvenido a CodeIgniter!</h1>
 
 	<div id="body">
-		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+		<p>La página que estas viendo es generada dinámicamente por CodeIgniter.</p>
 
-		<p>If you would like to edit this page you'll find it located at:</p>
+		<p>Si quisieras editar esta página la hallarás localizada en:</p>
 		<code>application/views/welcome_message.php</code>
 
-		<p>The corresponding controller for this page is found at:</p>
+		<p>El controlador correspondiente para esta página se encuentra en:</p>
 		<code>application/controllers/Welcome.php</code>
 
-		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+		<p>Si estas explorando CodeIgniter por primera vez, deberías comenzar leyendo la <a href="user_guide/">Guía de Usuario</a>.</p>
 	</div>
 
-	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+	<p class="footer">Página renderizada en <strong>{elapsed_time}</strong> segundos. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
 </div>
 
 </body>
