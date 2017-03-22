336b3fa37438946c8e4a0f61b2c4b4e1c8ecc5f1
Segundo-PC\Segundo <jose.soto.bit@gmail.com>
2017-03-22 00:48:14 -0430

    historias clinicas

--- application/controllers/HistoriaClinica.php
+++ application/controllers/HistoriaClinica.php
@@ -25,10 +25,10 @@ class HistoriaClinica extends CI_Controller {
 
         if (!$this->session->has_userdata('login') && ($this->uri->segment(1, 0) != '0' || $this->uri->segment(2, 0) != '0')) {
         	redirect(base_url());
-        }
-        if ($this->session->has_userdata('tipo_usuario') && $this->session->userdata('tipo_usuario') != "Administrador") {
+        }/*
+        if ($this->session->has_userdata('tipo_usuario') && ($this->session->userdata('tipo_usuario') != "Doctor" || $this->session->userdata('tipo_usuario') != "Enfermero")) {
         	redirect(base_url('Home')); 
-        }
+        }*/
     }
 
     /**
@@ -42,10 +42,15 @@ class HistoriaClinica extends CI_Controller {
      * @return void
      */
     public function ListarHistorias()
-    {
+    {/*
     	$condicion = array(
-			"select" => "historia.cod_historia, historia.fecha_creada, paciente.id, paciente.nombre1, paciente.nombre2, paciente.apellido1, paciente.apellido1",
+			"select" => "historia.cod_historia, historia.fecha_creada, paciente.id, paciente.nombre1, paciente.nombre2, paciente.apellido1, paciente.apellido2",
 			"join" => array("paciente", "paciente.id = historia.id_paciente")
+			);*/
+		$condicion = array(
+			"query" => "SELECT historia.cod_historia, historia.fecha_creada, paciente.id, paciente.nombre1, paciente.nombre2, paciente.apellido1, paciente.apellido2
+				FROM historia_medicina AS historia
+				JOIN paciente ON paciente.id = historia.id_paciente;"
 			);
 
 		$data = array();
